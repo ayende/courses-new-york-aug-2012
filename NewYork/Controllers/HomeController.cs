8153bd492e20fadc570efcac78973628c1d7730d
Sean Feldman <feldman.sean@gmail.com>
2012-08-21 09:46:21 -0600

    Create player

--- NewYork/Controllers/HomeController.cs
+++ NewYork/Controllers/HomeController.cs
@@ -3,16 +3,26 @@ using NewYork.Models;
 
 namespace NewYork.Controllers
 {
-	public class HomeController : RavenController
-	{
-		 public ActionResult Team(string name)
-		 {
-			 var entity = new Team
-				 {
-					 Name = name
-				 };
-			 Session.Store(entity);
-			 return Json(entity.Id);
-		 }
-	}
+  public class HomeController : RavenController
+  {
+    public ActionResult Team(string name)
+    {
+      var entity = new Team
+        {
+          Name = name
+        };
+      Session.Store(entity);
+      return Json(entity.Id);
+    }
+
+    public ActionResult Player(string name)
+    {
+      var player = new Player
+        {
+          Name = name
+        };
+      Session.Store(player);
+      return Json(player);
+    }
+  }
 }
